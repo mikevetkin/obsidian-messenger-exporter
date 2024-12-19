bc904c6babc314e6164607af10ab6af4fa289d9e
Michael <mvetkin@ozon.ru>
2024-12-19 17:39:24 +0300

    Added context menu item

--- main.ts
+++ main.ts
@@ -1,17 +1,6 @@
-import { fromMarkdown } from "mdast-util-from-markdown";
-import { toTelegram } from "mdast-util-to-telegram";
-import {
-  App,
-  Editor,
-  MarkdownView,
-  Modal,
-  Notice,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-} from "obsidian";
-
-// Remember to rename these classes and interfaces!
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { toTelegram } from 'mdast-util-to-telegram';
+import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface ObsidianTelegramPluginSettings {
   mySetting: string;
@@ -22,153 +11,173 @@ const DEFAULT_SETTINGS: ObsidianTelegramPluginSettings = {
 };
 
 export default class ObsidianTelegramPlugin extends Plugin {
-  settings: ObsidianTelegramPluginSettings;
+	settings: ObsidianTelegramPluginSettings;
 
-  async onload() {
-    await this.loadSettings();
+	async onload() {
+		await this.loadSettings();
 
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon(
-      "dice",
-      "Obsidian-Telegram",
-      (evt: MouseEvent) => {
-        // Called when the user clicks the icon.
-        const ast = fromMarkdown("### Hello world!");
-        const telegram = toTelegram(ast);
-        new Notice(telegram);
-      }
-    );
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass("my-plugin-ribbon-class");
+		/**
+		 * This creates an icon in the left ribbon.
+		 */
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian-Telegram', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			const ast = fromMarkdown("### Hello vladkuz!");
+			const telegram = toTelegram(ast);
+			new Notice(telegram);
+		});
 
-    /**
-     * This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-     */
+		/**
+		 * Perform additional things with the ribbon
+		 */
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-    // const statusBarItemEl = this.addStatusBarItem();
-    // statusBarItemEl.setText('Status Bar Text');
+		/**
+		 * This creates line in the "file-menu"
+		 */
+		this.registerEvent(
+			this.app.workspace.on('file-menu', (menu, file) => {
+			  menu.addItem((item) => {
+				item
+				  .setTitle('Export to Telegram')
+				  .setIcon('export')
+				  .onClick(async () => {
+					new Notice(file.path);
+				  });
+			  });
+			})
+		  );
 
-    /**
-     * This adds a simple command that can be triggered anywhere
-     */
+		/**
+		 * This adds a status bar item to the bottom of the app.
+		 * Does not work on mobile apps.
+		 */
 
-    // this.addCommand({
-    // 	id: 'open-sample-modal-simple',
-    // 	name: 'Open sample modal (simple)',
-    // 	callback: () => {
-    // 		new SampleModal(this.app).open();
-    // 	}
-    // });
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
-    /**
-     * This adds an editor command that can perform some operation on the current editor instance
-     */
+		/**
+		 * This adds a simple command that can be triggered anywhere
+		 */
 
-    // this.addCommand({
-    // 	id: 'sample-editor-command',
-    // 	name: 'Sample editor command',
-    // 	editorCallback: (editor: Editor, view: MarkdownView) => {
-    // 		console.log(editor.getSelection());
-    // 		editor.replaceSelection('Sample Editor Command');
-    // 	}
-    // });
+		// this.addCommand({
+		// 	id: 'open-sample-modal-simple',
+		// 	name: 'Open sample modal (simple)',
+		// 	callback: () => {
+		// 		new SampleModal(this.app).open();
+		// 	}
+		// });
 
-    /**
-     * This adds a complex command that can check whether the current state of the app allows execution of the command
-     */
 
-    // this.addCommand({
-    // 	id: 'open-sample-modal-complex',
-    // 	name: 'Open sample modal (complex)',
-    // 	checkCallback: (checking: boolean) => {
-    // 		// Conditions to check
-    // 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    // 		if (markdownView) {
-    // 			// If checking is true, we're simply "checking" if the command can be run.
-    // 			// If checking is false, then we want to actually perform the operation.
-    // 			if (!checking) {
-    // 				new SampleModal(this.app).open();
-    // 			}
+		/**
+		 * This adds an editor command that can perform
+		 * some operation on the current editor instance
+		 */
 
-    // 			// This command will only show up in Command Palette when the check function returns true
-    // 			return true;
-    // 		}
-    // 	}
-    // });
+		// this.addCommand({
+		// 	id: 'sample-editor-command',
+		// 	name: 'Sample editor command',
+		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
+		// 		console.log(editor.getSelection());
+		// 		editor.replaceSelection('Sample Editor Command');
+		// 	}
+		// });
 
-    /**
-     * This adds a settings tab so the user can configure various aspects of the plugin
-     */
+		/**
+		 * This adds a complex command that can check whether the current state of the app allows execution of the command
+		 */
 
-    // this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addCommand({
+		// 	id: 'open-sample-modal-complex',
+		// 	name: 'Open sample modal (complex)',
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
 
-    /**
-     * If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-     * Using this function will automatically remove the event listener when this plugin is disabled.
-     */
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 	}
+		// });
 
-    // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-    // 	console.log('click', evt);
-    // });
+		/**
+		 * This adds a settings tab so the user can configure various aspects of the plugin
+		 */
 
-    /**
-     * When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-     */
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 
-    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-  }
+		/**
+		 * If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		 * Using this function will automatically remove the event listener when this plugin is disabled.
+		 */
 
-  onunload() {}
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
+		/**
+		 * When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		 */
 
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
 
-  onOpen() {
-    const { contentEl } = this;
-    contentEl.setText("Woah!");
-  }
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
 
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
-class SampleSettingTab extends PluginSettingTab {
-  plugin: ObsidianTelegramPlugin;
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: ObsidianTelegramPlugin;
 
-  constructor(app: App, plugin: ObsidianTelegramPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
+// 	constructor(app: App, plugin: ObsidianTelegramPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
 
-  display(): void {
-    const { containerEl } = this;
+// 	display(): void {
+// 		const {containerEl} = this;
 
-    containerEl.empty();
+// 		containerEl.empty();
 
-    new Setting(containerEl)
-      .setName("Setting #1")
-      .setDesc("It's a secret")
-      .addText((text) =>
-        text
-          .setPlaceholder("Enter your secret")
-          .setValue(this.plugin.settings.mySetting)
-          .onChange(async (value) => {
-            this.plugin.settings.mySetting = value;
-            await this.plugin.saveSettings();
-          })
-      );
-  }
-}
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
