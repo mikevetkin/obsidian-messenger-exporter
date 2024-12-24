10857a04b9515bdcbe80dba8e0f23227baef4d8d
Michael Vetkin <mikevetkin@gmail.com>
2024-12-25 00:15:22 +0300

    Cleared up

--- main.ts
+++ main.ts
@@ -2,32 +2,22 @@ import { fromMarkdown } from 'mdast-util-from-markdown';
 import { toTelegram } from 'mdast-util-to-telegram';
 import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-const markdownToTelegramHTML = (markdown: string) => {
-    // Преобразуем Markdown ссылки в Telegram-совместимый HTML
-    const htmlContent = markdown.replace(
+const makeTelegramClipboardItem = (telegramMessage: string): ClipboardItem => {
+	const htmlContent = telegramMessage.replace(
         /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
         `<a class="text-entity-link" href="$2" data-entity-type="MessageEntityTextUrl" dir="auto">$1</a>`
     );
 
-    // Удаляем Markdown форматирование для plain text
-    const plainText = markdown.replace(
+    const plainText = telegramMessage.replace(
         /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
         '$1 ($2)'
     );
 
-    // Создаем объект для буфера обмена
-    const clipboardContent = new ClipboardItem({
+    return new ClipboardItem({
         'text/plain': new Blob([plainText], { type: 'text/plain' }),
         'text/html': new Blob([htmlContent], { type: 'text/html' })
-    });
-
-    // Записываем в буфер обмена
-    navigator.clipboard.write([clipboardContent]).then(() => {
-        console.log('Текст успешно скопирован в буфер обмена!');
-    }).catch((err) => {
-        console.error('Ошибка копирования в буфер обмена:', err);
-    });
-};
+    })
+}
 
 interface ObsidianTelegramPluginSettings {
   mySetting: string;
@@ -45,21 +35,18 @@ export default class ObsidianTelegramPlugin extends Plugin {
 
 		const markdownText = file && await this.app.vault.read(file);
 	
-
 		if (markdownText) {
 			const ast = fromMarkdown(markdownText);
 
 			const telegramMessage = toTelegram(ast);
-			// navigator.clipboard.writeText(telegramMessage);
 
-			// const htmlContent = telegramMessage.replace(
-			// 	/\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
-			// 	'<a class="text-entity-link" data-vladkuzator="pridurok" data-entity-type="MessageEntityUrl" dir="auto" href="https://mikevetkin.com">$1</a>'
-			// );
+			const clipboardContent = makeTelegramClipboardItem(telegramMessage);
 
-			markdownToTelegramHTML(telegramMessage);
-
-			new Notice("Text was copied in Telegram message format!")
+			navigator.clipboard.write([clipboardContent]).then(() => {
+				new Notice("Text was copied in Telegram message format!");
+			}).catch((err) => {
+				new Notice("Could not export text to Telegram");
+			})
 		} else {
 			new Notice("Could not export text to Telegram")
 		}
