0067a7d9b77a586800497cfcda53822a65d88dcd
Michael Vetkin <mikevetkin@gmail.com>
2024-12-25 02:34:35 +0300

    Added prettier

--- main.ts
+++ main.ts
@@ -1,181 +1,183 @@
 import { fromMarkdown } from 'mdast-util-from-markdown';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore:next-line
 import { toTelegram } from 'mdast-util-to-telegram';
-import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
 
 const makeTelegramClipboardItem = (telegramMessage: string): ClipboardItem => {
-	const htmlContent = telegramMessage.replace(
-        /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
-        `<a class="text-entity-link" href="$2" data-entity-type="MessageEntityTextUrl" dir="auto">$1</a>`
-    );
+  const htmlContent = telegramMessage.replace(
+    /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
+    `<a class="text-entity-link" href="$2" data-entity-type="MessageEntityTextUrl" dir="auto">$1</a>`,
+  );
 
-    const plainText = telegramMessage.replace(
-        /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
-        '$1 ($2)'
-    );
+  const plainText = telegramMessage.replace(
+    /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
+    '$1 ($2)',
+  );
 
-    return new ClipboardItem({
-        'text/plain': new Blob([plainText], { type: 'text/plain' }),
-        'text/html': new Blob([htmlContent], { type: 'text/html' })
-    })
-}
+  return new ClipboardItem({
+    'text/plain': new Blob([plainText], { type: 'text/plain' }),
+    'text/html': new Blob([htmlContent], { type: 'text/html' }),
+  });
+};
 
 interface ObsidianTelegramPluginSettings {
   mySetting: string;
 }
 
 const DEFAULT_SETTINGS: ObsidianTelegramPluginSettings = {
-  mySetting: "default",
+  mySetting: 'default',
 };
 
 export default class ObsidianTelegramPlugin extends Plugin {
-	settings: ObsidianTelegramPluginSettings;
+  settings: ObsidianTelegramPluginSettings;
 
-	private async perform() {
-		const file = this.app.workspace.getActiveFile();
+  private async perform() {
+    const file = this.app.workspace.getActiveFile();
 
-		const markdownText = file && await this.app.vault.read(file);
-	
-		if (markdownText) {
-			const ast = fromMarkdown(markdownText);
+    const markdownText = file && (await this.app.vault.read(file));
 
-			const telegramMessage = toTelegram(ast);
+    if (markdownText) {
+      const ast = fromMarkdown(markdownText);
 
-			const clipboardContent = makeTelegramClipboardItem(telegramMessage);
+      const telegramMessage = toTelegram(ast);
 
-			navigator.clipboard.write([clipboardContent]).then(() => {
-				new Notice("Text was copied in Telegram message format!");
-			}).catch((err) => {
-				new Notice("Could not export text to Telegram");
-			})
-		} else {
-			new Notice("Could not export text to Telegram")
-		}
-	}
+      const clipboardContent = makeTelegramClipboardItem(telegramMessage);
 
-	async onload() {
-		await this.loadSettings();
+      navigator.clipboard
+        .write([clipboardContent])
+        .then(() => {
+          new Notice('Text was copied in Telegram message format!');
+        })
+        .catch((err) => {
+          new Notice('Could not export text to Telegram');
+        });
+    } else {
+      new Notice('Could not export text to Telegram');
+    }
+  }
 
-		/**
-		 * This creates an icon in the left ribbon.
-		 */
+  async onload() {
+    await this.loadSettings();
 
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian-Telegram', (evt: MouseEvent) => {
-		// 	this.perform();
-		// });
+    /**
+     * This creates an icon in the left ribbon.
+     */
 
-		/**
-		 * Perform additional things with the ribbon
-		 */
+    // const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian-Telegram', (evt: MouseEvent) => {
+    // 	this.perform();
+    // });
 
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
+    /**
+     * Perform additional things with the ribbon
+     */
 
-		/**
-		 * This creates line in the "file-menu"
-		 */
-		this.registerEvent(
-			this.app.workspace.on('file-menu', (menu, file) => {
-			menu.addItem((item) => {
-				item
-				.setTitle('Export to Telegram')
-				.setIcon('export')
-				.onClick(async () => {
-					this.perform();
-				});
-			});
-		})
-	);
+    // ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		/**
-		 * This adds a status bar item to the bottom of the app.
-		 * Does not work on mobile apps.
-		 */
+    /**
+     * This creates line in the "file-menu"
+     */
+    this.registerEvent(
+      this.app.workspace.on('file-menu', (menu, file) => {
+        menu.addItem((item) => {
+          item
+            .setTitle('Export to Telegram')
+            .setIcon('export')
+            .onClick(async () => {
+              this.perform();
+            });
+        });
+      }),
+    );
 
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
+    /**
+     * This adds a status bar item to the bottom of the app.
+     * Does not work on mobile apps.
+     */
 
-		/**
-		 * This adds a simple command that can be triggered anywhere
-		 */
+    // const statusBarItemEl = this.addStatusBarItem();
+    // statusBarItemEl.setText('Status Bar Text');
 
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
+    /**
+     * This adds a simple command that can be triggered anywhere
+     */
 
+    // this.addCommand({
+    // 	id: 'open-sample-modal-simple',
+    // 	name: 'Open sample modal (simple)',
+    // 	callback: () => {
+    // 		new SampleModal(this.app).open();
+    // 	}
+    // });
 
-		/**
-		 * This adds an editor command that can perform
-		 * some operation on the current editor instance
-		 */
+    /**
+     * This adds an editor command that can perform
+     * some operation on the current editor instance
+     */
 
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
+    // this.addCommand({
+    // 	id: 'sample-editor-command',
+    // 	name: 'Sample editor command',
+    // 	editorCallback: (editor: Editor, view: MarkdownView) => {
+    // 		console.log(editor.getSelection());
+    // 		editor.replaceSelection('Sample Editor Command');
+    // 	}
+    // });
 
-		/**
-		 * This adds a complex command that can check whether the current state of the app allows execution of the command
-		 */
+    /**
+     * This adds a complex command that can check whether the current state of the app allows execution of the command
+     */
 
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
+    // this.addCommand({
+    // 	id: 'open-sample-modal-complex',
+    // 	name: 'Open sample modal (complex)',
+    // 	checkCallback: (checking: boolean) => {
+    // 		// Conditions to check
+    // 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    // 		if (markdownView) {
+    // 			// If checking is true, we're simply "checking" if the command can be run.
+    // 			// If checking is false, then we want to actually perform the operation.
+    // 			if (!checking) {
+    // 				new SampleModal(this.app).open();
+    // 			}
 
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
+    // 			// This command will only show up in Command Palette when the check function returns true
+    // 			return true;
+    // 		}
+    // 	}
+    // });
 
-		/**
-		 * This adds a settings tab so the user can configure various aspects of the plugin
-		 */
+    /**
+     * This adds a settings tab so the user can configure various aspects of the plugin
+     */
 
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
+    // this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		/**
-		 * If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		 * Using this function will automatically remove the event listener when this plugin is disabled.
-		 */
+    /**
+     * If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+     * Using this function will automatically remove the event listener when this plugin is disabled.
+     */
 
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
+    // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+    // 	console.log('click', evt);
+    // });
 
-		/**
-		 * When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		 */
+    /**
+     * When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+     */
 
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  }
 
-	onunload() {
+  // onunload() {}
 
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 // class SampleModal extends Modal {
